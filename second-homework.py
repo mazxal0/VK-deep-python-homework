9939fad6a4454c24c3aaceb7b8181146d1561bad
mazxal <makwest5008@gmail.com>
2026-03-16 13:47:41 +0300

    finish second step of second homework

--- second-homework.py
+++ second-homework.py
@@ -10,6 +10,15 @@ class Stack:
     def __init__(self) -> None:
         self._stack: List[Any] = []
 
+    def __len__(self) -> int:
+        return len(self._stack)
+
+    def __str__(self) -> str:
+        return f'Stack({", ".join(map(str, self._stack))})'
+
+    def __repr__(self) -> str:
+        return f"Stack({self._stack})"
+
     def push(self, value: Any) -> None:
         self._stack.append(value)
 
