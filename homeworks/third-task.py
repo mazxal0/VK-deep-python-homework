eb898cfe945b5124a4eba68c4e028bc7f02e61db
mazxal <makwest5008@gmail.com>
2026-03-24 21:06:02 +0300

    second step of third homework

--- homeworks/third-task.py
+++ homeworks/third-task.py
@@ -1,14 +1,43 @@
+from typing import Any
+
+
 class TypedProperty:
-    def __init__(self, _type):
+    def __init__(self, _type) -> None:
         self._type = _type
 
-    def __set_name__(self, owner, name):
+    def __set_name__(self, owner, name) -> None:
         self._name = name
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance, owner) -> Any:
         return instance.__dict__[self._name]
 
-    def __set__(self, instance, value):
+    def __set__(self, instance, value) -> None:
         if not isinstance(value, self._type):
             raise TypeError('%r should be other type: %r' % (self._name, self._type))
+        instance.__dict__[self._name] = value
+
+class ValidatedProperty(TypedProperty):
+    def __init__(self, expected_type, _max=None, _min=None, min_length=None) -> None:
+        super().__init__(expected_type)
+        self._max = _max
+        self._min = _min
+        self._min_length = min_length
+
+    def __get__(self, instance, value) -> Any:
+        return instance.__dict__[self._name]
+
+    def __set__(self, instance, value) -> None:
+        if not isinstance(value, self._type):
+            raise TypeError(f'{self._name} should be type like: {self._type}')
+
+        if isinstance(value, int):
+            if self._min and value < self._min:
+                raise ValueError(f'{self._name} must be >= {self._min}')
+            if self._max and value > self._max:
+                raise ValueError(f'{self._name} must be <= {self._max}')
+
+        if isinstance(value, str):
+            if self._min_length and len(value) < self._min_length:
+                raise ValueError(f'len of {self._name} must be >= {self._min_length}')
+
         instance.__dict__[self._name] = value
